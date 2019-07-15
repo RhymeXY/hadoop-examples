5617d8ea90430701c541ad0b00986d67d90e3a9e
RhymeXY <xy_rhyme@163.com>
2019-07-15 21:50:12 +0800

    feat: WordCount例子

---
+++ src/main/java/com/xy/hadoop/mapreduce/WordCount.java
@@ -0,0 +1,49 @@
+package com.xy.hadoop.mapreduce;
+
+import com.xy.hadoop.pojo.MyMapper;
+import com.xy.hadoop.pojo.MyReducer;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+
+public class WordCount {
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        Configuration conf = new Configuration();
+        // 创建一个job
+        Job job = Job.getInstance(conf);
+
+        job.setJarByClass(WordCount.class);
+
+        job.setJobName("wordCountJob");
+
+        // 设置需要处理的文件路径
+        Path inPath = new Path("/yg/haha/hello.txt");
+        FileInputFormat.addInputPath(job, inPath);
+
+        // 设置job完成后输出的文件路径
+        Path outputPath = new Path("/output/wordcount");
+        if (outputPath.getFileSystem(conf).exists(outputPath)) {
+            // 路径如果先存在就删除
+            outputPath.getFileSystem(conf).delete(outputPath, true);
+        }
+
+        FileOutputFormat.setOutputPath(job, outputPath);
+
+        job.setMapperClass(MyMapper.class);
+        // 设置Map处理后，输出的数据类型，reducer用来进行反序列化
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+        job.setReducerClass(MyReducer.class);
+
+        job.waitForCompletion(true);
+
+    }
+
+}
---
+++ src/main/java/com/xy/hadoop/pojo/MyMapper.java
@@ -0,0 +1,24 @@
+package com.xy.hadoop.pojo;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class MyMapper extends Mapper<Object, Text, Text, IntWritable> {
+
+    private static final IntWritable one = new IntWritable(1);
+
+    private Text word = new Text();
+
+    @Override
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+        StringTokenizer stringTokenizer = new StringTokenizer(value.toString());
+        while (stringTokenizer.hasMoreTokens()) {
+            word.set(stringTokenizer.nextToken());
+            context.write(word, one);
+        }
+    }
+}
